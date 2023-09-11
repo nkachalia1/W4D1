2c79af3f638a16c887a69d2b167ea94e2822138a
Balaji <imnotbalaji@gmail.com>
2023-09-11 16:46:57 -0400

    Completing phase 1

--- practice-for-ch-ruby-knights-travails-long-practice-main/Knight_Traversal.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/Knight_Traversal.rb
@@ -1,21 +1,10 @@
 require_relative 'searchable.rb'
 
 class KnightPathFinder
+  
 
-    attr_reader @considered_positions
-
-    def initialize(starting_position)
-        @root_node = PolyTreeNode.new(starting_position)
-        make_tree(@root_node)
-        @considered_positions = [starting_position]
-    end
-
-    def new_move_positions(pos)
-    end
-
-    def make_tree(node)
-        current_position = node.value
-        current_row, current_col = current_position
+    def self.new_move_positions(pos)
+        current_row, current_col = pos
         new_pos = []
         (-2..2).each do |row|
             (-2..2).each do |col|
@@ -28,13 +17,38 @@ class KnightPathFinder
         end
 
         new_pos.reject! { |el| el.any? { |i| i < 1 || i > 8 } }
-        new_pos.reject! { |el| @considered_positions.include?(el) }
+  
 
-        return nil if new_pos.empty?
+        return new_pos
+    end 
+    
+    attr_reader :considered_positions
 
-        new_pos.each do |el|
-            child_node = PolyTreeNode.new(el)
-            make_tree(child_node)
-            node.add_child(child_node)
+    def initialize(starting_position)
+        @root_node = PolyTreeNode.new(starting_position)
+        @queue = []
+        @queue << @root_node
+        @considered_positions = [starting_position]
+        self.make_tree
+    end
+   
+
+    def make_tree
+        until @queue.empty? 
+            head = @queue.shift
+            current_position = head.value
+            new_pos = KnightPathFinder.new_move_positions(current_position)
+            new_pos.reject! { |el| @considered_positions.include?(el) }
+
+            @considered_positions += new_pos
+
+            new_pos.each do |el|
+                child_node = PolyTreeNode.new(el)
+                head.add_child(child_node)
+                @queue << child_node
+            end 
+        end 
+    end 
 
 end
+
